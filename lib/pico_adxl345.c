d7d8af546b9ddf519fe4c7ebe4ea20aca9b2314a
zNitche <53448802+zNitche@users.noreply.github.com>
2026-01-30 22:06:53 +0100

    feat: proper setting measurements range

--- lib/pico_adxl345.c
+++ lib/pico_adxl345.c
@@ -73,17 +73,11 @@ void adxl345_get_settings(ADXL345I2C i2c_c, uint8_t* buff) {
 
 void adxl345_set_measurements_range(ADXL345I2C i2c_c,
                                     enum ADXL345Ranges range) {
-  // unsigned int range_d0 = (range >> 1) & 1;
-  // unsigned int range_d1 = (range >> 0) & 1;
-
-  // ToDo set proper range
-  // 0b00001011
-  uint8_t settings_buff = 0b00001001;
+  uint8_t settings_buff = 0b00001000;
+  settings_buff += range;
 
   write_to_reg(i2c_c, ADXL345_REGISTERS.DATA_FORMAT_REGISTER_ADDR,
                settings_buff);
-
-  adxl345_get_settings(i2c_c, &settings_buff);
 }
 
 void adxl345_start_measurements(ADXL345I2C i2c_c) {
