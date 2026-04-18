f8277f373a2d38c9018f60e5965d6a6220b2f4ed
zNitche <53448802+zNitche@users.noreply.github.com>
2026-04-18 19:45:55 +0000

    refactor: example.c - code formatting

--- src/example.c
+++ src/example.c
@@ -1,45 +1,46 @@
 #include <stdio.h>
 
-#include "pico_adxl345/adxl345.h"
-#include "pico_adxl345/defines.h"
 #include "hardware/i2c.h"
 #include "pico/stdlib.h"
+#include "pico_adxl345/adxl345.h"
+#include "pico_adxl345/defines.h"
 
 int main() {
-  stdio_init_all();
+    stdio_init_all();
 
 #ifdef DEBUG
-  // waiting for input
-  getchar();
+    // waiting for input
+    getchar();
 #endif
 
-  ADXL345I2C adxl345_i2c = {i2c0, PICO_ADXL345_I2C_DEFAULT_ADDRESS, 0, 1};
+    ADXL345I2C adxl345_i2c = {i2c0, PICO_ADXL345_I2C_DEFAULT_ADDRESS, 0, 1};
 
-  adxl345_setup_i2c(adxl345_i2c);
-  sleep_ms(250);
+    adxl345_setup_i2c(adxl345_i2c);
+    sleep_ms(250);
 
-  bool connected = adxl345_check_connection(adxl345_i2c);
+    bool connected = adxl345_check_connection(adxl345_i2c);
 
-  printf("connected: %d\n", connected);
+    printf("connected: %d\n", connected);
 
-  adxl345_set_measurements_range(adxl345_i2c, ADXL345_RANGE_4G);
+    adxl345_set_measurements_range(adxl345_i2c, ADXL345_RANGE_4G);
 
-  adxl345_start_measurements(adxl345_i2c);
+    adxl345_start_measurements(adxl345_i2c);
 
-  if (connected == true) {
-    printf("processing...\n");
-    float accel[3] = {0.0};
+    if (connected == true) {
+        printf("processing...\n");
+        float accel[3] = {0.0};
 
-    while (true) {
-      adxl345_get_readings(adxl345_i2c, accel);
+        while (true) {
+            adxl345_get_readings(adxl345_i2c, accel);
 
-      printf("Acc. X = %f, Y = %f, Z = %f\n", accel[0], accel[1], accel[2]);
+            printf("Acc. X = %f, Y = %f, Z = %f\n", accel[0], accel[1],
+                   accel[2]);
 
-      sleep_ms(200);
+            sleep_ms(200);
+        }
+
+        adxl345_stop_measurements(adxl345_i2c);
     }
 
-    adxl345_stop_measurements(adxl345_i2c);
-  }
-
-  return 0;
+    return 0;
 }
